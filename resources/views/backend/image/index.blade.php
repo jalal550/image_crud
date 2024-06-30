7b2f9ad18097061dc720d94e9f3bc9ec7fafc351
Jalal Miah <60940248+jalal550@users.noreply.github.com>
2024-06-30 14:28:30 +0600

    Update index.blade.php

--- resources/views/backend/image/index.blade.php
+++ resources/views/backend/image/index.blade.php
@@ -7,67 +7,6 @@ all images
 @section('body')
 
 
-<section class="py-5">
-{{--        <div class="container">--}}
-{{--            <div class="row">--}}
-
-{{--                <div class="row">--}}
-{{--                    <div class=col-md-12 >--}}
-{{--                        <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">--}}
-{{--                            <ol class="breadcrumb">--}}
-{{--                                <li class="breadcrumb-item"><a href="#">Product Management</a></li>--}}
-{{--                                <li class="breadcrumb-item active" aria-current="page">All Products</li>--}}
-{{--                            </ol>--}}
-{{--                        </nav>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div>--}}
-{{--                    <h6>Image Upload with AJAX</h6>--}}
-{{--                    <form id="imageUploadForm" enctype="multipart/form-data">--}}
-{{--                        <div class="dropzone" id="imageDropzone"></div>--}}
-{{--                        <button class="btn btn-success mt-2" type="submit">Upload</button>--}}
-{{--                    </form>--}}
-{{--                    <h6>Image Show</h6>--}}
-{{--                    <div id="images"></div>--}}
-
-
-
-{{--                    <div>--}}
-{{--                </div>--}}
-{{--                <div class="col-md-12 mx-auto">--}}
-{{--                    <div class="card-body">--}}
-
-{{--                        @if(session()->has('success'))--}}
-{{--                            <div class="alert alert-success">--}}
-{{--                                {{ session('success') }}--}}
-{{--                            </div>--}}
-{{--                        @endif--}}
-
-{{--                        <table class="table" id="productTable">--}}
-{{--                            <thead>--}}
-{{--                                <tr>--}}
-{{--                                    <th>Serial</th>--}}
-
-{{--                                    <th>Image</th>--}}
-{{--                                    <th>Uploaded On</th>--}}
-{{--                                    <th style="width: 60px;">Action</th>--}}
-{{--                                </tr>--}}
-{{--                            </thead>--}}
-{{--                            <tbody>--}}
-
-
-
-
-{{--                            </tbody>--}}
-{{--                        </table>--}}
-
-{{--                      <div>--}}
-{{--                      </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
 
     <div class="container">
         <h6>Image Upload with AJAX</h6>
